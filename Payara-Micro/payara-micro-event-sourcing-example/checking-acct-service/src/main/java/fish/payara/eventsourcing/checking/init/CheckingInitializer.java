211bbef43aeac07eced0f4d27c391f0e59f994df
Eric Tiongson <eric@tiongson.ph>
2018-06-18 16:36:54 +0800

    changed jdbc datasource to use postgres in RDS

--- Payara-Micro/payara-micro-event-sourcing-example/checking-acct-service/src/main/java/fish/payara/eventsourcing/checking/init/CheckingInitializer.java
+++ Payara-Micro/payara-micro-event-sourcing-example/checking-acct-service/src/main/java/fish/payara/eventsourcing/checking/init/CheckingInitializer.java
@@ -31,7 +31,9 @@ import org.h2.tools.Server;
 @DataSourceDefinition(
         // global to circumvent https://java.net/jira/browse/GLASSFISH-21447
         name = "java:global/savingsDS",
-        className = "org.h2.jdbcx.JdbcDataSource",
+        className = "org.postgresql.Driver",
+        url = "jdbc:postgresql://ndbench-postgres.cqbpwgwhtywd.us-west-2.rds.amazonaws.com:5432/banking_db?" +
+                "user=cfs_dev&password=somepassword&ssl=true"
         url = "jdbc:h2:~/checking_db;DB_CLOSE_ON_EXIT=TRUE;FILE_LOCK=NO"
 )
 
